f025d047b13dbe3de399cab7235947fe5dbc0d4c
kkpandatime <103954582+kkpandatime@users.noreply.github.com>
2022-06-17 23:41:33 +0300

    Tasks 1-5

---
+++ L5/Ex_1-2.py
@@ -0,0 +1,31 @@
+'''
+1. Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
+odd_to_15 = odd_nums(15)
+next(odd_to_15)
+1
+next(odd_to_15)
+3
+...
+next(odd_to_15)
+15
+next(odd_to_15)
+...StopIteration...
+
+'''
+def odd_num(num):
+    for i in range(1, num, 2):
+        yield i
+
+gen_odd_num = odd_num(7)
+print(next(gen_odd_num))
+print(next(gen_odd_num))
+print(next(gen_odd_num))
+
+
+'''
+2. Решить задачу генерации нечётных чисел от 1 до n (включительно), не используя ключевое слово yield.
+'''
+
+gen_out_yield = (i for i in range(1,34,2))
+for i in gen_out_yield:
+    print(i)
